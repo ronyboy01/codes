fbff133050ca541327d54606903ef356eafaa1c2
Mihir Gupta <62780056+MihirGupta07@users.noreply.github.com>
2024-10-17 21:13:29 +0530

    Create LargestSwap.cpp

---
+++ LargestSwap.cpp
@@ -0,0 +1,27 @@
+//Problem Link
+//https://www.geeksforgeeks.org/problems/largest-number-in-one-swap1520/0
+
+string LargestSwap(string S) {
+        int n = S.length();
+        
+        // Store the last occurrence of each digit in the string
+        vector<int> last(10, -1);
+        for (int i = 0; i < n; i++) {
+            last[S[i] - '0'] = i;
+        }
+        
+        // Traverse through the string to find the best swap
+        for (int i = 0; i < n; i++) {
+            for (int d = 9; d > S[i] - '0'; d--) {
+                // Check if there exists a digit larger than S[i] and it occurs later
+                if (last[d] > i) {
+                    // Swap the two digits
+                    swap(S[i], S[last[d]]);
+                    return S;
+                }
+            }
+        }
+        
+        // If no swap can make it larger, return the original string
+        return S;
+    }
